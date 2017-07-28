8d99c4a45a9784e15d1974e3688a3424d33cb79e
叶振荣 <zryea@qiyeclass.com>
2017-07-28 14:42:35 +0800

    2017年7月28日14:38:18更新

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,7 +2,6 @@
 
 namespace Home\Controller;
 
-use Think\Controller;
 
 class IndexController extends BaseController
 {
@@ -12,13 +11,8 @@ class IndexController extends BaseController
 //        $replace = array($this->getAppid(), $this->getAppsecret(), '011LX2ti2lBuSE0L8Lti2rvsti2LX2ts');
 //        $url = str_replace($search, $replace, $this->getWxGetOpenUrl());
 //        dump($url);
-        $url='https://api.weixin.qq.com/sns/jscode2session?appid=wx6fc5f4836fc51f82&secret=aab9cf6c1f3de3f225d787afb097e40d&js_code=001Wsqyt1Qa1bb0O5bAt1efHyt1WsqyM&grant_type=authorization_code';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx6fc5f4836fc51f82&secret=aab9cf6c1f3de3f225d787afb097e40d&js_code=001HHmtq00MqKq1fDxvq00ebtq0HHmtH&grant_type=authorization_code';
+        $result = sendPost($url);
         dump($result);
     }
 }
