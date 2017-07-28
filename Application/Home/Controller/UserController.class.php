d7283734a239c1a637ad0bba42edb35e5176c1e2
叶振荣 <zryea@qiyeclass.com>
2017-07-28 14:53:06 +0800

    2017年7月28日14:50:50更新

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -21,10 +21,11 @@ class UserController extends BaseController
 //        $search = array("APPID", 'SECRET', 'JSCODE');
 //        $replace = array($this->getAppid(), $this->getAppsecret(), $code);
 //        $url = str_replace($search, $replace, $this->getWxGetOpenUrl());
+        dump('code-----' . $code);
         $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx6fc5f4836fc51f82&secret=aab9cf6c1f3de3f225d787afb097e40d&js_code=' . $code . '&grant_type=authorization_code';
 
         $wxObject = sendPost($url);
-
+        dump('wxObject:' . $wxObject);
         //判断用户是否存在
         if ($this->checkUser($wxObject['openid'])) {
             $data['token'] = $this->getWxToken();
