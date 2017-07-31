368206b9f300cd3a3870199e870c9f6d22a50a3a
zryea <zryea@qiyeclass.com>
2017-07-31 14:55:42 +0800

    修改了主页

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,17 +9,6 @@ class IndexController extends BaseController
 {
     public function index()
     {
-        if (IS_POST) {
-            $config = C('UPLOAD_SITEIMG_QINIU');
-            $upload = new Qiniu($config['driverConfig']);
-            $info = $upload->save($_FILES['file']);
-            if ($info) {
-                dump($_FILES['file']['name'] . '上传成功');
-            } else {
-                dump($upload->getError());
-
-            }
-        }
         $this->display();
     }
 }
