007aa3cb52f4042a674db585f81b893b4d68589d
zryea <zryea@qiyeclass.com>
2017-07-31 09:30:44 +0800

    复位RestController

--- ThinkPHP/Library/Think/Controller/RestController.class.php
+++ ThinkPHP/Library/Think/Controller/RestController.class.php
@@ -200,7 +200,7 @@ class RestController extends Controller
         if (empty($data)) return '';
         if ('json' == $type) {
             // 返回JSON数据格式到客户端 包含状态信息
-            $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+            $data = json_encode($data);
         } elseif ('xml' == $type) {
             // 返回xml格式数据
             $data = xml_encode($data);
